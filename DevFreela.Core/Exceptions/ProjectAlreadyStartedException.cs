3f90c255afb3ca952f9c29de78f7dcc487b6dae5
Rodrigo Bertozzi de Castro <rbertozzicastro1@hotmail.com>
2022-08-15 10:51:39 -0300

    Adicionado o DevFreela.Core no projeto

---
+++ DevFreela.Core/Exceptions/ProjectAlreadyStartedException.cs
@@ -0,0 +1,10 @@
+﻿namespace DevFreela.Core.Exceptions
+{
+    public class ProjectAlreadyStartedException : Exception
+    {
+        public ProjectAlreadyStartedException() : base("Project already in Started status")
+        {
+
+        }
+    }
+}
