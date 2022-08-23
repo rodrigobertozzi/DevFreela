2a9d17b0183f774b689113fdfd3b04369903a4c6
Rodrigo Bertozzi de Castro <rbertozzicastro1@hotmail.com>
2022-08-22 23:53:41 -0300

    Refatorando as queries para usar o Padrão Repository

--- DevFreela.API/Program.cs
+++ DevFreela.API/Program.cs
@@ -2,6 +2,8 @@ using DevFreela.Infrastructure.Persistence;
 using MediatR;
 using DevFreela.Application.Commands.CreateProject;
 using Microsoft.EntityFrameworkCore;
+using DevFreela.Core.Repositories;
+using DevFreela.Infrastructure.Persistence.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +12,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("DevFreelaCs"); 
 builder.Services.AddDbContext<DevFreelaDbContext>(option => option.UseSqlServer(connectionString));
+
+builder.Services.AddScoped<IProjectRepository, ProjectRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<ISkillRepository, SkillRepository>();
 
 builder.Services.AddControllers();
 builder.Services.AddMediatR(typeof(CreateProjectCommand));
